cb9ad01f37dfbd2be5f8e97cb80a038eb21cacb3
Mustafa Tevfik Kadan <mtevfik41@gmail.com>
2022-03-01 16:16:57 +0300

    initial commit

---
+++ src/main/java/com/softtech/case3mtevfik41/Case3Mtevfik41Application.java
@@ -0,0 +1,13 @@
+package com.softtech.case3mtevfik41;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Case3Mtevfik41Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Case3Mtevfik41Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/softtech/case3mtevfik41/Case3Mtevfik41ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softtech.case3mtevfik41;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Case3Mtevfik41ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
